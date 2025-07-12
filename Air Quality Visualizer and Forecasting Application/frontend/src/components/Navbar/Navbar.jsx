55c31c491ab282b39c9426efa14f4fa9a92804b5
Sangram Das <sangramraju143@gmail.com>
2025-07-13 01:43:31 +0530

    Navbar updates

--- Air Quality Visualizer and Forecasting Application/frontend/src/components/Navbar/Navbar.jsx
+++ Air Quality Visualizer and Forecasting Application/frontend/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.css";
+import { Airplay, Github, Facebook, Twitter } from "lucide-react";
 
 const Navbar = () => {
   return (
-   <div>
+    <>
+    <nav className="nav">
+        <div className="left">PROJECT X</div>
+        <div className="mid">
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Service</a></li>
+            <li><a href="">Contact Us</a></li>
+          </ul>
+        </div>
+        <div className="right">
+          <ul>
+            <li><Airplay className="w-6 h-6 text-orange-600" /></li>
+            <li> <Github className="w-6 h-6 text-orange-600" /></li>
+            <li><Facebook className="w-6 h-6 text-orange-600" /></li>
+            <li><Twitter className="w-6 h-6 text-orange-600" /></li>
+          </ul>
+        </div>
+      </nav>
 
-    <a href="">Home</a>
-    <a href="">About</a>
-    <a href="">Service</a>
-    <a href="">Contact Us</a>
+    </>
 
-    <div>
-      
-    </div>
-   </div>
-  )
-}
+  );
+};
 
-
-
-export default Navbar
+export default Navbar;
